a68cff2f7aed32411f567951cc45746ec0b866a6
Alexey Krasman <akrasman@gmail.com>
2020-04-18 16:59:39 +0300

    hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/p2p' },
-  { path: 'p2p', loadChildren: () => import('./p2p/p2p.module').then(m => m.P2pModule) },
-];
+  { path: 'p2p', loadChildren: () => import('./p2p/p2p.module').then((m) => m.P2pModule) },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
